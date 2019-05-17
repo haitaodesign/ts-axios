1d9438b3e42358746f22504ae64c00d13c873289
haitaodesign <15393243613@163.com>
2019-05-17 08:22:49 +0800

    feat: 比较复杂的继承例子

--- class.ts
+++ class.ts
@@ -16,8 +16,34 @@ let greeter = new Greeter('LeeHaiTao')
 // 继承，使用继承来扩展现有的类
 
 class Animal {
+  name: string
+  constructor (name: string) {
+    this.name = name
+  }
   move (distance: number = 0) {
-    console.log(`Animal moved ${distance}`)
+    console.log(`${this.name} moved ${distance}`)
+  }
+}
+
+class Snake extends Animal {
+  constructor (name: string) {
+    // 在构造器里访问 this 的属性之前，一定要调用 super()
+    // ts 强制执行的一条重要规则
+    super(name)
+  }
+  move (distance: number = 5) {
+    console.log('Slithering...')
+    super.move(distance)
+  }
+}
+
+class Horse extends Animal {
+  constructor (name: string) {
+    super(name)
+  }
+  move (distance: number = 5) {
+    console.log('Galloping...')
+    super.move(distance)
   }
 }
 
@@ -27,6 +53,12 @@ class Dog extends Animal {
   }
 }
 
-const dog = new Dog()
+const dog = new Dog('lee')
 dog.bark()
 dog.move(10)
+
+const sam = new Snake('Sammy')
+const tom: Animal = new Horse('Tommy')
+
+sam.move()
+tom.move(100)
