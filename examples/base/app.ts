06db8c8025426e5181d49812f9a938d77715df1b
haitaodesign <15393243613@163.com>
2019-05-25 10:49:45 +0800

    feat: 处理请求 header 以及 demo编写

--- examples/base/app.ts
+++ examples/base/app.ts
@@ -65,18 +65,25 @@ axios({
 axios({
   method: 'post',
   url: '/base/post',
+  headers: {
+    'content-type': 'application/json;'
+  },
   data: {
     a: 1,
     b: 2
   }
 })
 
-const arr = new Int32Array([21, 31])
+// const arr = new Int32Array([21, 31])
+
+const paramsString = 'q=URLUtils.searchParams&topic=api'
+
+const searchParams = new URLSearchParams(paramsString)
 
 axios({
   method: 'post',
   url: '/base/buffer',
-  data: arr
+  data: searchParams
 })
 
 
--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import { AxiosRequestConfig } from './types'
 import xhr from './xhr'
 import { buildURL } from './helpers/url'
 import { transformRequest } from './helpers/data'
+import { processHeaders } from './helpers/header'
 
 function axios(config: AxiosRequestConfig) {
   processConfig(config)
@@ -10,12 +11,22 @@ function axios(config: AxiosRequestConfig) {
 
 function processConfig(config: AxiosRequestConfig): void {
   config.url = transformUrl(config)
-  config.data = transformRequest(config)
+  config.headers = transformHeaders(config)
+  config.data = transformRequestData(config)
 }
 
 function transformUrl(config: AxiosRequestConfig): string {
   const { url, params } = config
   return buildURL(url, params)
+}
+
+function transformRequestData(config: AxiosRequestConfig): any {
+  return transformRequest(config.data)
+}
+
+function transformHeaders(config: AxiosRequestConfig) {
+  const { headers = {}, data } = config
+  return processHeaders(headers, data)
 }
 
 export default axios
