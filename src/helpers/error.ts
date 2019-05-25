d5c5f93bb8d6f265668717d7e5722f746aee44ee
haitaodesign <15393243613@163.com>
2019-05-25 14:13:00 +0800

    feat: 创建 AxiosError 类

---
+++ src/helpers/error.ts
@@ -0,0 +1,41 @@
+import { AxiosRequestConfig, AxiosResponse } from '../types'
+
+export class AxiosError extends Error {
+  config: AxiosRequestConfig
+  code?: string
+  request?: any
+  response?: AxiosResponse
+  isAxiosError: boolean
+
+  constructor(
+    message: string,
+    config: AxiosRequestConfig,
+    code?: string,
+    request?: any,
+    response?: AxiosResponse
+  ) {
+    super(message)
+
+    this.config = config
+    this.code = code
+    this.request = request
+    this.response = response
+    this.isAxiosError = true
+
+    // 解决 ts 继承原生对象的一些坑
+    //https://github.com/Microsoft/TypeScript-wiki/blob/master/Breaking-Changes.md#extending-built-ins-like-error-array-and-map-may-no-longer-work
+    Object.setPrototypeOf(this, AxiosError.prototype)
+  }
+}
+
+export function createError(
+  message: string,
+  config: AxiosRequestConfig,
+  code?: string,
+  request?: any,
+  response?: AxiosResponse
+): AxiosError {
+  const error = new AxiosError(message, config, code, request, response)
+
+  return error
+}
